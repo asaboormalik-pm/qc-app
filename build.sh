14b29b3ba16f5f6ec7b14938c34f9d8d3897ee9d
Tayyab <ariftayyab2002@gmail.com>
2026-04-07 22:07:29 +0530

    bump version to 1.1.9

--- build.sh
+++ build.sh
@@ -61,7 +61,7 @@ if [ -d "dist/QC Print Agent.app" ]; then
 
     # Create README for installer
     cat > installers/README.txt << 'EOF'
-QC Print Agent v1.1.8
+QC Print Agent v1.1.9
 ========================
 
 Installation:
